83e474a6ca222a2f5f718ac8998d22d39717bdb4
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-03-03 14:32:28 +1100

    Create enum file to use it in navebar and index files.

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { SelectedPage } from "@/shared/types";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 
-type Props = { selectedPage: string; setSelectedPage: (value: string) => void };
+type Props = {
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Navbar = (selectedPage, setSelectedPage: Props) => {
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
   return (
     <nav>
